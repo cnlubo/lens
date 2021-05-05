1a0bb608440c6753ff6dffad7073fa438f5aad56
Sebastian Malton <sebastian@malton.name>
2021-05-05 16:35:27 -0400

    Formalize the to/from Store rules for UserStore
    - Explicitly don't save to disk if the value in the store is the same as
      the default. That way we can change the defaults between versions
    
    - Calculate the PreferencesModel on demand
    
    - In the future, when we turn on strict null checking, we can enforce
      that the UserStore expects the correct types too
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/__tests__/user-store.test.ts
+++ src/common/__tests__/user-store.test.ts
@@ -20,6 +20,7 @@ import { SemVer } from "semver";
 import electron from "electron";
 import { stdout, stderr } from "process";
 import { beforeEachWrapped } from "../../../integration/helpers/utils";
+import { ThemeStore } from "../../renderer/theme.store";
 
 console = new Console(stdout, stderr);
 
@@ -64,7 +65,7 @@ describe("user store tests", () => {
       us.httpsProxy = "abcd://defg";
 
       expect(us.httpsProxy).toBe("abcd://defg");
-      expect(us.colorTheme).toBe(UserStore.defaultTheme);
+      expect(us.colorTheme).toBe(ThemeStore.defaultTheme);
 
       us.colorTheme = "light";
       expect(us.colorTheme).toBe("light");
@@ -77,7 +78,7 @@ describe("user store tests", () => {
 
       us.colorTheme = "some other theme";
       await us.resetTheme();
-      expect(us.colorTheme).toBe(UserStore.defaultTheme);
+      expect(us.colorTheme).toBe(ThemeStore.defaultTheme);
     });
 
     it("correctly calculates if the last seen version is an old release", () => {
